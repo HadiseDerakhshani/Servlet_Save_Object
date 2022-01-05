ac8cc51052386bd2901d650f56687c69b9db9d70
HadiseDerakhshani <sevensama0@gmail.com>
2022-01-05 19:38:31 +0330

    modify space between line

--- src/main/java/Save.java
+++ src/main/java/Save.java
@@ -17,22 +17,27 @@ public class Save extends HttpServlet {
         PrintWriter result = resp.getWriter();
         resp.setContentType("text/html");
         try {
-
             SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
             Visitor visitor = new Visitor();
             visitor.setNationalCode(Integer.parseInt(req.getParameter("code")));
             visitor.setName(req.getParameter("name"));
+
             Session session = sessionFactory.openSession();
             session.beginTransaction();
+
             session.persist(visitor);
+
             session.getTransaction().commit();
             session.close();
 
             result.println("<html><body><h1 style=\"text-align:center;\"> Successfully Inserted"
                     + "</h1></body></html>");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         result.close();
 
     }
